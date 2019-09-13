c74fc0427e61fa39096a883ed70feee778c415ae
Andrew Whitaker <andrew.whitaker@mac.com>
2019-09-12 17:50:21 -0700

    plotting

---
+++ plot.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+import csv
+
+x = []
+y = []
+
+with open('out.csv','r') as csvfile:
+    plots = csv.reader(csvfile, delimiter=',')
+    next(plots)
+    
+    for row in plots:
+        x.append(float(row[10]))
+        y.append(float(row[5]) / 10 **9)
+
+plt.plot(x,y)
+plt.xlabel('buffer size')
+plt.ylabel('memory bandwidth (GB)')
+plt.title('Memory bandwidth')
+plt.xscale('log')
+plt.show()
